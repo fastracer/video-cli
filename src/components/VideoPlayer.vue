e31cf61a20eb394fbd0547d8420a309b35de9e70
Eric <yhemogollon@gmail.com>
2020-06-29 19:11:37 -0500

    buttons add

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,91 +1,91 @@
 <template>
-	<div>
-		<div class="video-tool">
-			<video class="video-js" ref="videoPlayer"></video>
-		</div>
-	</div>
+    <div class="video-tool">
+        <video class="video-js" ref="videoPlayer" />
+        <pre>{{ setTimeValue }}</pre>
+    </div>
 </template>
 
 <script>
 import videojs from 'video.js'
 import 'video.js/dist/video-js.css'
+import offset from 'videojs-offset/dist/videojs-offset'
 
 export default {
-	name: 'VideoPlayer',
+    name: 'VideoPlayer',
 
-	props: ['setTimeValue', 'aspectRatio', 'startTime', 'endTime', 'poster', 'sourceData'],
+    props: ['setTimeValue', 'aspectRatio', 'startTime', 'endTime', 'poster', 'sourceData'],
 
-	data() {
-		return {
-			player: null,
-			options: {
-				aspectRatio: this.aspectRatio,
-				playbackRates: [0.5, 1.0, 1.5, 2.0],
-				autoplay: false,
-				controls: true,
-				controlBar: {
-					children: ['playToggle', 'volumePanel', 'playbackRateMenuButton', 'progressControl', 'remainingTimeDisplay', 'fullscreenToggle'],
-					pictureInPictureToggle: true,
-					volumePanel: {
-						inline: false
-					}
-				},
-				poster: this.poster,
-				preload: 'auto',
-				fluid: true,
-				sources: this.sourceData
-			}
-		}
-	},
+    data() {
+        return {
+            player: null,
+            options: {
+                aspectRatio: this.aspectRatio,
+                playbackRates: [0.5, 1.0, 1.5, 2.0],
+                autoplay: false,
+                controls: true,
+                controlBar: {
+                    children: ['playToggle', 'volumePanel', 'playbackRateMenuButton', 'progressControl', 'remainingTimeDisplay', 'fullscreenToggle'],
+                    pictureInPictureToggle: true,
+                    volumePanel: {
+                        inline: false
+                    }
+                },
+                poster: this.poster,
+                preload: 'auto',
+                fluid: true,
+                sources: this.sourceData
+            }
+        }
+    },
 
-	methods: {
-		async playVideo() {
-			this.player.offset({
-				start: this.setTimeValue[0].start,
-				end: this.setTimeValue[0].end,
-				restart_beginning: false
-			})
-			this.player.reset()
-			this.player.src(this.sourceData)
-			await this.player.play()
-		}
-	},
+    methods: {
+        async playVideo() {
+            this.player.offset({
+                start: this.setTimeValue[0].start,
+                end: this.setTimeValue[0].end,
+                restart_beginning: false
+            })
+            this.player.reset()
+            this.player.src(this.sourceData)
+            await this.player.play()
+        }
+    },
 
-	mounted() {
-		this.player = videojs(this.$refs.videoPlayer, this.options)
-		console.log(this.setTimeValue)
-	},
+    mounted() {
+        this.player = videojs(this.$refs.videoPlayer, this.options)
+        console.log(this.setTimeValue)
+    },
 
-	beforeDestroy() {
-		if (this.player) {
-			this.player.dispose()
-		}
-	}
+    beforeDestroy() {
+        if (this.player) {
+            this.player.dispose()
+        }
+    }
 }
 </script>
 
 <style>
 .video-js .vjs-big-play-button {
-	border-radius: 50%;
-	border: none;
-	font-size: 36px;
-	height: 80px;
-	left: 50%;
-	line-height: 2.2;
-	top: 50%;
-	transform: translate(-50%, -50%);
-	width: 80px;
+    border-radius: 50%;
+    border: none;
+    font-size: 36px;
+    height: 80px;
+    left: 50%;
+    line-height: 2.2;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 80px;
 }
 
 .video-js .vjs-control-bar {
-	background: #000;
-	margin-bottom: 20px;
+    background: #000;
+    margin-bottom: 20px;
 }
 
 .vjs-menu li.vjs-selected {
-	background: #000;
-	color: #fff;
-	padding-bottom: 5px;
-	padding-top: 5px;
+    background: #000;
+    color: #fff;
+    padding-bottom: 5px;
+    padding-top: 5px;
 }
 </style>
--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,60 +1,72 @@
 <template>
-  <div>
-    <VideoPlayer
-      :source-data="data.video_link"
-      :start-time="data.start"
-      :end-time="data.end"
-      :poster="data.poster"
-      :set-time-value="data.setTimeValue"
-      aspect-ratio="16:9"
-    />
-  </div>
+    <v-container>
+        <div class="page-tool">
+            <video-player
+                :end-time="data.end"
+                :poster="data.poster"
+                :set-time-value="data.setTimeValue"
+                :source-data="data.video_link"
+                :start-time="data.start"
+                aspect-ratio="16:9"
+            />
+            <template v-for="(value, index) in data.setTimeValue">
+                <div :key="index" class="button-player mb-4 mt-4">
+                    <v-btn @click="playerVideo()">Reproducir desde {{ value.start }}seg hasta {{ value.end }}seg</v-btn>
+                </div>
+            </template>
+        </div>
+    </v-container>
 </template>
 
 <script>
 import VideoPlayer from '@/components/VideoPlayer.vue'
 export default {
-	name: 'AppHome',
-	components: { VideoPlayer },
-	data() {
-		return {
-			data: {
-				id: 18,
-				name: 'video_name',
-				description: 'video_description',
-				date: '2020-05-27T07:39:38.000Z',
-				related_to: 'Competitions',
-				video_type: 'Full Match',
-				season: '2018-2019',
-				competition_name: 'Premiere League 2018-2019',
-				game_id: 1,
-				video_link: [
-					{
-						src: 'https://videos-adc.s3.amazonaws.com/GMT20200425-000626_Mejorando-_1600x800.mp4',
-						type: 'video/mp4',
-						label: '480P'
-					},
-					{
-						src: 'https://videos-adc.s3.amazonaws.com/GMT20200425-000626_Mejorando-_1600x800.mp4',
-						type: 'video/mp4',
-						label: '720P'
-					},
-					{
-						src: 'https://videos-adc.s3.amazonaws.com/GMT20200425-000626_Mejorando-_1600x800.mp4',
-						type: 'video/mp4',
-						label: '1080P'
-					}
-				],
-				setTimeValue: [
-					{ start: 120, end: 240 },
-					{ start: 240, end: 320 },
-					{ start: 320, end: 480 },
-					{ start: 480, end: 520 }
-				],
-				poster: 'https://worldsoccertalk.com/wp-content/uploads/2014/11/scotland-england.jpg',
-				keywords: '#liverpool =fullmatch !1280x720'
-			}
-		}
-	}
+    name: 'AppHome',
+    components: { VideoPlayer },
+    data() {
+        return {
+            data: {
+                id: 18,
+                name: 'video_name',
+                description: 'video_description',
+                date: '2020-05-27T07:39:38.000Z',
+                related_to: 'Competitions',
+                video_type: 'Full Match',
+                season: '2018-2019',
+                competition_name: 'Premiere League 2018-2019',
+                game_id: 1,
+                video_link: [
+                    {
+                        src: 'https://videos-adc.s3.amazonaws.com/GMT20200425-000626_Mejorando-_1600x800.mp4',
+                        type: 'video/mp4',
+                        label: '480P'
+                    },
+                    {
+                        src: 'https://videos-adc.s3.amazonaws.com/GMT20200425-000626_Mejorando-_1600x800.mp4',
+                        type: 'video/mp4',
+                        label: '720P'
+                    },
+                    {
+                        src: 'https://videos-adc.s3.amazonaws.com/GMT20200425-000626_Mejorando-_1600x800.mp4',
+                        type: 'video/mp4',
+                        label: '1080P'
+                    }
+                ],
+                setTimeValue: [
+                    { start: 120, end: 240 },
+                    { start: 240, end: 320 },
+                    { start: 320, end: 480 },
+                    { start: 480, end: 520 }
+                ],
+                poster: 'https://worldsoccertalk.com/wp-content/uploads/2014/11/scotland-england.jpg',
+                keywords: '#liverpool =fullmatch !1280x720'
+            }
+        }
+    },
+    methods: {
+        playerVideo() {
+            console.log('play')
+        }
+    }
 }
 </script>
